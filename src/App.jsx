1af0d4bd03cea1da64da934283d958c8dff23434
abautista <alexjav.12@hotmail.es>
2023-11-28 23:46:04 -0600

    add the component ItemListCotiner and css for each component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <ItemListContainer greeting="Hola Mundo" />
     </div>
   );
 };
