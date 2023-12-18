bafac5bae482dfb659dd74d1717defd7515b3483
abautista <alexjav.12@hotmail.es>
2023-12-17 23:28:31 -0600

    install the library react-router

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { getUnProducto } from "../../asyncmock";
 import ItemDetail from "../ItemDetail/ItemDetail";
+import { useParams } from "react-router-dom";
 
 const ItemDetailContainer = () => {
   return <div>ItemDetailContainer</div>;
