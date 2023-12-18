b612a7e0aabd229ca7ca7dae701b950898ffeaa9
abautista <alexjav.12@hotmail.es>
2023-12-17 22:50:21 -0600

    Start of Pre Entrega 2

---
+++ src/asyncmock.js
@@ -0,0 +1,21 @@
+const productos = [
+    {id: 1, nombre: "Necklace 1", precio: 500, img: "./img/prod1.jpeg"}
+    {id: 2, nombre: "Necklace 2", precio: 500, img: "./img/prod1.jpeg"}
+    {id: 3, nombre: "Ring 1", precio: 500, img: "./img/prod1.jpeg"}
+    {id: 4, nombre: "Water Bottle 1", precio: 500, img: "./img/prod1.jpeg"}
+]
+
+
+export const getProducts = () => {
+  return new Promise(resolve => {
+    setTimeout(() =>{
+        resolve(productos)
+    }, 2000)
+  })
+}
+
+/* Actividad N°3 Detalle de un producto I*/
+
+/*Crear una función similar a la anterior pero que solo nos retorne un item */
+
+
