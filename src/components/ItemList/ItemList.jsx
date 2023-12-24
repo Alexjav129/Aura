f4decc86be372a20c2407d7a3f92b064e7e9a654
abautista <alexjav.12@hotmail.es>
2023-12-23 20:31:03 -0600

    add the ItemList component in order to display the products on the home page

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Item from "../Item/item";
+import "./ItmeList.scss";
 
-const ItemList = () => {
-  return <div>ItemList</div>;
+const ItemList = ({ productos }) => {
+  return (
+    <div className="contenedorProductos">
+      {productos.map((producto) => (
+        <Item key={producto.id} {...producto} />
+      ))}
+    </div>
+  );
 };
 
 export default ItemList;
