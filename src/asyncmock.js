c67d8b9989cee918bb7cb914c834fe545eda69cb
abautista <alexjav.12@hotmail.es>
2023-12-23 19:23:18 -0600

    finished the ItemListContainer component

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,30 +1,64 @@
 const productos = [
-    {id: 1, nombre: "Necklace 1", precio: 500, img: "./img/prod1.jpeg"},
-    {id: 2, nombre: "Necklace 2", precio: 500, img: "./img/prod1.jpeg"},
-    {id: 3, nombre: "Ring 1", precio: 500, img: "./img/prod1.jpeg"},
-    {id: 4, nombre: "Water Bottle 1", precio: 500, img: "./img/prod1.jpeg"},
-]
-
+  {
+    id: "1",
+    nombre: "Necklace 1",
+    precio: 500,
+    img: "./img/prod1.jpeg",
+    idCat: "2",
+  },
+  {
+    id: "2",
+    nombre: "Necklace 2",
+    precio: 500,
+    img: "./img/prod1.jpeg",
+    idCat: "2",
+  },
+  {
+    id: "3",
+    nombre: "Ring 1",
+    precio: 500,
+    img: "./img/prod1.jpeg",
+    idCat: "3",
+  },
+  {
+    id: "4",
+    nombre: "Water Bottle 1",
+    precio: 500,
+    img: "./img/prod1.jpeg",
+    idCat: "3",
+  },
+];
 
 export const getProducts = () => {
-  return new Promise(resolve => {
-    setTimeout(() =>{
-        resolve(productos)
-    }, 2000)
-  })
-}
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(productos);
+    }, 2000);
+  });
+};
 
 /* Actividad N°3 Detalle de un producto I*/
 
 /*Crear una función similar a la anterior pero que solo nos retorne un item */
 
 export const getUnProducto = (id) => {
-    return new Promise(resolve => {
-        setTimeout( () =>  {
-            const producto = productos.find(item => item.id === id);
-            resolve(producto);
-        }, 2000 )
-    })
-}
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const producto = productos.find((item) => item.id === id);
+      resolve(producto);
+    }, 2000);
+  });
+};
 
+// Se tiene que crear una nueva función que retorne toda un categoría
 
+export const getProductosPorCategoria = (idCategoria) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const productosCategoria = productos.filter(
+        (item) => item.idCat === idCategoria
+      );
+      resolve(productosCategoria);
+    }, 2000);
+  });
+};
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
+import { getProducts, getProductosPorCategoria } from "../../asyncmock";
+import ItemList from "../ItemList/ItemList";
+import { useParams } from "react-router-dom";
 import "./ItemListContianer.scss";
 
 const ItemListContainer = ({ greeting }) => {
-  return <h2>{greeting}</h2>;
+  const [productos, setProductos] = useState([]);
+
+  const { idCategoria } = useParams();
+
+  useEffect(() => {
+    const funcionProductos = idCategoria
+      ? getProductosPorCategoria
+      : getProducts;
+
+    funcionProductos(idCategoria).then((respuesta) => setProductos(respuesta));
+  }, [idCategoria]);
+
+  return (
+    <div>
+      <h2>Mis Productos</h2>
+      <ItemList productos={productos} />
+    </div>
+  );
 };
 
 export default ItemListContainer;
