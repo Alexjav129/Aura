10751a706bc212d86189e3f96bfbad87277d9465
abautista <alexjav.12@hotmail.es>
2024-01-07 21:48:16 -0600

    install firebase, create the services folder and create the config.js with the firebase config

---
+++ src/services/config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYwdVR-tvNqjECQxMMMBhDvdPvSCATFv8",
+  authDomain: "marolio-forever-b2574.firebaseapp.com",
+  projectId: "marolio-forever-b2574",
+  storageBucket: "marolio-forever-b2574.appspot.com",
+  messagingSenderId: "19098434442",
+  appId: "1:19098434442:web:ff8372f72db1d8f7eae673",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFireStore(app);
