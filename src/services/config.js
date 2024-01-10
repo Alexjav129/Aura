894034f5e9a7a0598eead95aeb93ce354b71c266
abautista <alexjav.12@hotmail.es>
2024-01-09 23:40:17 -0600

    add some coments to the config.js file

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore"; // Add this line
 
 const firebaseConfig = {
   apiKey: "AIzaSyDYwdVR-tvNqjECQxMMMBhDvdPvSCATFv8",
@@ -12,4 +12,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = getFirestore(app); // Add this line
