2498b77d97ff946f3e0d244147f5affd79aa45b8
abautista <alexjav.12@hotmail.es>
2024-01-21 22:54:49 -0600

    finished with the logic of the CarritoContext component

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,5 +1,24 @@
-const CartItem = () => {
-  return <div>CartItem</div>;
+import { useContext } from "react";
+import { CarritoContext } from "../../context/CarritoContext";
+
+const CartItem = ({ item, cantidad }) => {
+  const { eliminarProducto } = useContext(CarritoContext);
+
+  return (
+    <div>
+      <h4> {item.nombre}</h4>
+      <p>Quantity: {cantidad}</p>
+      <p>Precio: {item.precio}</p>
+      <button
+        onClick={() => {
+          eliminarProducto(item.id);
+        }}
+      >
+        Delete Product
+      </button>
+      <hr />
+    </div>
+  );
 };
 
 export default CartItem;
