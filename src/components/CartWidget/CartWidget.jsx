60c60d0fe5026bb65d2433f42d7109c7461ef13a
abautista <alexjav.12@hotmail.es>
2024-01-21 18:37:36 -0600

    add some media queries for the navbar

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -8,7 +8,7 @@ const CartWidget = () => {
         src="./img/carrito.png"
         alt="Shopping Cart Widget"
       />
-      <strong>7</strong>
+      <strong className="cartNumber">7</strong>
     </div>
   );
 };
