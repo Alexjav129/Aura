82e05e436e79b113c08cd7ceb8f93292270c1a7a
abautista <alexjav.12@hotmail.es>
2024-01-25 20:09:48 -0600

    add some styles to the header and buttons

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,15 +1,17 @@
 import { useContext } from "react";
 import { CarritoContext } from "../../context/CarritoContext";
+import "./CartItem.scss";
 
 const CartItem = ({ item, cantidad }) => {
   const { eliminarProducto } = useContext(CarritoContext);
 
   return (
-    <div>
+    <div className="cartContent">
       <h4> {item.nombre}</h4>
       <p>Quantity: {cantidad}</p>
       <p>Price per product: {item.precio}</p>
       <button
+        className="btn"
         onClick={() => {
           eliminarProducto(item.id);
         }}
