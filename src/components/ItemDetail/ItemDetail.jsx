135b08ee781043cd9073529f3042aca93a8140eb
abautista <alexjav.12@hotmail.es>
2024-01-25 21:31:37 -0600

    add styles to some paragraphs and update the name of the products in Firebase

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -29,22 +29,24 @@ const ItemDetail = ({ id, nombre, stock, precio, img }) => {
       <h2>Product: {nombre} </h2>
       <p>Price: {precio} </p>
       <p>ID: {id}</p>
-      <p>
+      <p className="p1">
         Aura bracelets transcend mere jewelry; they symbolize a philosophy
         centered on equilibrium, consciousness, and self-reflection.
       </p>
-      <p>
+      <p className="p2">
         Embracing Harmony: In moments of triumph and accomplishment, Aura
         encourages staying grounded, fostering humility and appreciation,
         reminding us of our origins and the journey traveled.
       </p>
-      <p>
+      <p className="p1">
         Aura bracelets act as daily cues for mindfulness, prompting us to
         embrace the present, treasure the peaks, and endure the valleys of life.
       </p>
       <img src={img} alt={nombre} />
       {agregarCantidad > 0 ? (
-        <Link to="/cart">Terminar Compra</Link>
+        <Link className="terminarCompra" to="/cart">
+          Terminar Compra
+        </Link>
       ) : (
         <ItemCount
           inicial={1}
